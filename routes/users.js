9948e0cb1ef701b1dff91b3ec4e9cdbf918d8836
Time <244439359@qq.com>
2019-08-22 10:44:34 +0800

    增加user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/in_memo/user')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/', function (req, res, next) {
+  const u = new User(req.body.name, req.body.age)
+  res.locals.user = u
+  res.render('user')
 });
 
 module.exports = router;
